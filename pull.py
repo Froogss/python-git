15ff411983a83d269210683f3c6473d8f6af64f9
Froogss <fakerstuna@gmail.com>
2018-03-20 03:28:54 +0000

    Update pull.py

--- pull.py
+++ pull.py
@@ -1,8 +1,6 @@
 from subprocess import call
 
-repo = "git@github.com:Froogss/testman-sam2.git"
 
-pull_path = r"~/Desktop/git_pulls"
 
 def pull(repo, location):
-  call("git pull {} {}".format(repo, pull_path))
+  call("git pull {} {}".format(repo, location))
